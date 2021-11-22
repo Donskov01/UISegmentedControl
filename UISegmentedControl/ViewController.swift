de5fb61df56b1b37b994119825b5de0d34d5ca0e
Fedor Donskov <fedya_donskov@mail.ru>
2021-11-22 06:03:42 +0300

    Initial Commit

---
+++ UISegmentedControl/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  UISegmentedControl
+//
+//  Created by Fedor Donskov on 21.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        slider.value = 1
+        
+        label.text = String(slider.value)
+        label.font = label.font.withSize(35)
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        
+        segmentedControl.insertSegment(withTitle: "Third", at: 2, animated: true)
+        
+        slider.minimumValue = 0
+        slider.maximumValue = 1
+        slider.minimumTrackTintColor = .yellow
+        slider.maximumTrackTintColor = .red
+        slider.thumbTintColor = .blue
+    }
+
+    @IBAction func choiseSegment(_ sender: UISegmentedControl) {
+        
+        label.isHidden = false
+        
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            label.text = "The first segment is selected"
+            label.textColor = .red
+        case 1:
+            label.text = "The second segment is selected"
+            label.textColor = .blue
+        case 2:
+            label.text = "The third segment is selected"
+            label.textColor = .yellow
+        default:
+            print("Sothing wrong!")
+        }
+    }
+    
+    @IBAction func sliderAction(_ sender: UISlider) {
+        label.text = String(sender.value)
+        
+        let backgrandColor = self.view.backgroundColor
+        self.view.backgroundColor = backgrandColor?.withAlphaComponent(CGFloat(sender.value))
+    }
+}
+
